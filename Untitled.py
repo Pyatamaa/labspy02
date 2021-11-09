8f079cf8187b94b61931dbb66122f72e538decc4
Hp <nurpratama001@gmail.com>
2021-11-09 08:21:44 -0800

    Untitled.py telah ditambahkan

---
+++ Untitled.py
@@ -0,0 +1,14 @@
+
+print(20*"_-_")
+
+print ("masukkan nilai n: 5")
+import random
+jumlah = 5
+a = 0
+for x in range (jumlah):
+    i = random.uniform (.0,.5)
+    a+=1
+    print("data ke=", a,"===>",i)
+    print("selesai")
+
+print(20*"-_-")    
