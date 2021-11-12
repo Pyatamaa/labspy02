42024f92e6d6c57883753774979883ffad9f3544
Hp <nurpratama001@gmail.com>
2021-11-11 22:12:25 -0800

    saya menambahkan Latihan2.py

---
+++ Latihan2.py
@@ -0,0 +1,13 @@
+#masukkan bilangan antara terbesar dan terkecil dalam bentuk satuan atau puluhan atau ratusan
+
+k = input("masukkan biangan 1 = ")
+l = input("masukkan biangan 1 = ")
+m = input("masukkan biangan 1 = ")
+n = input("masukkan biangan 1 = ")
+o = input("masukkan biangan 1 = ")
+p = input("masukkan biangan 1 = ")
+q = input("masukkan biangan 1 = ")
+
+data = [k,l,m,n,o,p,q]
+
+print("bilangan terbesar adalah : ", max(data))
